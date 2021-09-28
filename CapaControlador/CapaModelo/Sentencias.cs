a15e8eccff96c067c70bf2b762f73754c28bcf4b
Ashly2015 <ranbr200015@gmail.com>
2021-09-28 10:26:50 -0600

    sentencias

--- CapaControlador/CapaModelo/Sentencias.cs
+++ CapaControlador/CapaModelo/Sentencias.cs
@@ -1,12 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CapaModelo
 {
-    class Sentencias
+    public class Sentencias
     {
+        private Conexion cn = new Conexion();
+        private OdbcCommand Comm;
+
+     
+        //mantenimiento Linea
+
+        public void funInsertar(string Id, string Nombre, int estado)
+        {
+            string cadena = "INSERT INTO" +
+            " `sic2`.`lineas` VALUES (" + "'" + Id + "', '" + Nombre + "' , " + estado + ");";
+
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+        }
+
+        public void funModificar(string Id, string Nombre, int estado)
+        {
+            string cadena = "UPDATE sic2.linea set codigo_linea ='" + Id
+              + "',nombre ='" + Nombre + "',estado = " + estado + "  where pkId= '" + Id + "';";
+
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+        }
+
+        public void funEliminarLinea(string Id)
+        {
+            string cadena = "delete from sic2.linea where codigo_linea ='" + Id + "';";
+
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+        }
+
+        public (string, int) funBuscar(string id, string nombre, int estado)
+        {
+            string Query = "select * from `sic2`.`linea` where codigo_linea='" + id + "';";
+
+            OdbcCommand consulta = new OdbcCommand(Query, cn.conexion());
+            consulta.ExecuteNonQuery();
+
+            OdbcDataReader busqueda;
+            busqueda = consulta.ExecuteReader();
+
+            if (busqueda.Read())
+            {
+                nombre = busqueda["nombre_linea"].ToString();
+                estado = int.Parse(busqueda["estatus_linea"].ToString());
+            }
+
+            return (nombre, estado);
+        }
+
+
+
+        //mantenimiento Producto
+        public OdbcDataReader llenarcbxLinea(string sql)
+        {
+            try
+            {
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader llenarcbxMarca(string sql)
+        {
+            try
+            {
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        
+        public string consultaLinea(string nombre)
+        {
+
+            string id = "";
+            string Query = "select * from `sic2`.`lineas` where nombre_linea='" + nombre + "';";
+
+            OdbcCommand consulta = new OdbcCommand(Query, cn.conexion());
+            consulta.ExecuteNonQuery();
+
+            OdbcDataReader busqueda;
+            busqueda = consulta.ExecuteReader();
+
+            if (busqueda.Read())
+            {
+
+                id = busqueda["codigo_linea"].ToString();
+
+            }
+
+
+            return id;
+        }
+
+        public string consultaMarca(string nombre)
+        {
+
+            string id = "";
+            string Query = "select * from `sic2`.`marcas` where nombre_marca='" + nombre + "';";
+
+            OdbcCommand consulta = new OdbcCommand(Query, cn.conexion());
+            consulta.ExecuteNonQuery();
+
+            OdbcDataReader busqueda;
+            busqueda = consulta.ExecuteReader();
+
+            if (busqueda.Read())
+            {
+
+                id = busqueda["codigo_marca"].ToString();
+
+            }
+
+
+            return id;
+        }
+
+        
+
+        
     }
 }
+
--- CapaControlador/CapaVista/frmLinea.Designer.cs
+++ CapaControlador/CapaVista/frmLinea.Designer.cs
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtEstatus = new System.Windows.Forms.TextBox();
+            this.gbxEstado = new System.Windows.Forms.GroupBox();
+            this.btnInactivo = new System.Windows.Forms.RadioButton();
+            this.btnActivo = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.codigo_linea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre_linea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estatus_linea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gbxEstado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(338, 39);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 13;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(117, 39);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Castellar", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Castellar", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Codigo";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnBuscar.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBuscar.Location = new System.Drawing.Point(459, 33);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 30);
+            this.btnBuscar.TabIndex = 22;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnLimpiar.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpiar.Location = new System.Drawing.Point(459, 249);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 30);
+            this.btnLimpiar.TabIndex = 21;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEliminar.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEliminar.Location = new System.Drawing.Point(459, 191);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 30);
+            this.btnEliminar.TabIndex = 20;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnModificar.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModificar.Location = new System.Drawing.Point(459, 144);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(100, 30);
+            this.btnModificar.TabIndex = 19;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnInsertar.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInsertar.Location = new System.Drawing.Point(459, 89);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(100, 30);
+            this.btnInsertar.TabIndex = 18;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = false;
+            // 
+            // txtEstatus
+            // 
+            this.txtEstatus.Location = new System.Drawing.Point(260, 127);
+            this.txtEstatus.Name = "txtEstatus";
+            this.txtEstatus.Size = new System.Drawing.Size(28, 20);
+            this.txtEstatus.TabIndex = 24;
+            // 
+            // gbxEstado
+            // 
+            this.gbxEstado.Controls.Add(this.btnInactivo);
+            this.gbxEstado.Controls.Add(this.btnActivo);
+            this.gbxEstado.Font = new System.Drawing.Font("Castellar", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxEstado.Location = new System.Drawing.Point(41, 92);
+            this.gbxEstado.Name = "gbxEstado";
+            this.gbxEstado.Size = new System.Drawing.Size(200, 66);
+            this.gbxEstado.TabIndex = 23;
+            this.gbxEstado.TabStop = false;
+            this.gbxEstado.Text = "Estatus";
+            // 
+            // btnInactivo
+            // 
+            this.btnInactivo.AutoSize = true;
+            this.btnInactivo.Location = new System.Drawing.Point(17, 38);
+            this.btnInactivo.Name = "btnInactivo";
+            this.btnInactivo.Size = new System.Drawing.Size(95, 17);
+            this.btnInactivo.TabIndex = 5;
+            this.btnInactivo.TabStop = true;
+            this.btnInactivo.Text = "Inactivo";
+            this.btnInactivo.UseVisualStyleBackColor = true;
+            // 
+            // btnActivo
+            // 
+            this.btnActivo.AutoSize = true;
+            this.btnActivo.Location = new System.Drawing.Point(115, 38);
+            this.btnActivo.Name = "btnActivo";
+            this.btnActivo.Size = new System.Drawing.Size(79, 17);
+            this.btnActivo.TabIndex = 6;
+            this.btnActivo.TabStop = true;
+            this.btnActivo.Text = "Activo";
+            this.btnActivo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo_linea,
+            this.nombre_linea,
+            this.estatus_linea});
+            this.dataGridView1.Location = new System.Drawing.Point(41, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(347, 150);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // codigo_linea
+            // 
+            this.codigo_linea.HeaderText = "codigo_linea";
+            this.codigo_linea.Name = "codigo_linea";
+            // 
+            // nombre_linea
+            // 
+            this.nombre_linea.HeaderText = "nombre_linea";
+            this.nombre_linea.Name = "nombre_linea";
+            // 
+            // estatus_linea
+            // 
+            this.estatus_linea.HeaderText = "estatus_linea";
+            this.estatus_linea.Name = "estatus_linea";
+            // 
+            // frmLinea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtEstatus);
+            this.Controls.Add(this.gbxEstado);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLinea";
             this.Text = "frmLinea";
+            this.gbxEstado.ResumeLayout(false);
+            this.gbxEstado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox txtEstatus;
+        private System.Windows.Forms.GroupBox gbxEstado;
+        private System.Windows.Forms.RadioButton btnInactivo;
+        private System.Windows.Forms.RadioButton btnActivo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo_linea;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_linea;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estatus_linea;
     }
 }
